2fb456c987ec5827cdcc6f761b94b419b7c014ac
Mohammad Faisal <faisalkhan9354@gmail.com>
2023-12-03 11:05:24 +0530

    port and cors fixed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // Configure CORS
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  };
+
+  app.enableCors(corsOptions);
+  await app.listen(port);
+  console.log(`App listening on port ${port}`);
 }
 bootstrap();
